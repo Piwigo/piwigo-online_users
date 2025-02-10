4febd94c6ecd6f2e939f9a96f4d1031829e3c2d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-10 05:20:30 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Users online'] = '在线用户';
+$lang['guests'] = '访客';
