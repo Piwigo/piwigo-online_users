7ff9db84fd4a82594c2cd162ee8d1d883da8b550
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-11 12:44:26 +0100

    Update cs_CZ, thanks to webprostor

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Users online'] = 'Uživatelů online';
+$lang['guests'] = 'návštěv';
